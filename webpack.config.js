2c1aa8a6e245235a1aa816d813018402b7c63a9b
hoangtvh <tranvanhuyhoang@gmail.com>
2023-01-04 18:03:05 +0700

    set up webpack run html

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: 'index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'index.js'),
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Root config',
-    template: "index.html",
-    filename: 'index.html',
-  })],
-  module: {
-    rules: [
-      // JavaScript
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-    ],
-  }
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3001,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Root config',
+      filename: 'index.html',
+      template: './template.html'
+    })
+  ],
+  // module: {
+  //   rules: [
+  //     // JavaScript
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: ['babel-loader'],
+  //     },
+  //     {
+  //       test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     {
+  //       test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+  //       type: 'asset/inline',
+  //     },
+  //   ],
+  // }
 };
