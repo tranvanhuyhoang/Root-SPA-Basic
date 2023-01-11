ea73263882571cae8348dec141f162edc6b697aa
hoangtvh <tranvanhuyhoang@gmail.com>
2023-01-11 22:01:00 +0700

    feat: set up navbar in root-file

--- src/mfe-root-config.js
+++ src/mfe-root-config.js
@@ -3,12 +3,18 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
     name: "@mfe/react-app",
     app: () => System.import("@mfe/react-app"),
-    activeWhen: ["/"],
+    activeWhen: ["/react"],
 });
 
 registerApplication({
     name: "@mfe/vue-app",
     app: () => System.import("@mfe/vue-app"),
+    activeWhen: ["/vue"],
+});
+
+registerApplication({
+    name: "@mfe/react-navbar",
+    app: () => System.import("@mfe/react-navbar"),
     activeWhen: ["/"],
 });
 
